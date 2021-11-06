5f76d9730c869d2597d13a17d0fce563db3ef966
PedroMurilloArnau <Pedro.marnau@gmail.com>
2021-11-06 17:54:43 +0100

    implemento la suma de los elementos

--- js/precio.js
+++ js/precio.js
@@ -1,9 +1,26 @@
+function calcuprecio(){
+var total = 0
+if(pequeña.checked){
+    total = total + 5
+}
+if(mediana.checked){
+    total = total + 10
+}
+if(grande.checked){
+    total = total + 15
+}
+var sum = document.getElementsByClassName("checkbox")
+for( i=0; i< sum.length;++i){
+    if(sum[i].checked){
+        total = total + 1
+    }
 
+}
+
+alert("El precio total es de " + total+ " €")  
+    
+}
 window.onload = function(){
     formulario.onsubmit = calcuprecio;//si la funcion validacion me retorna true, mando la 
                                     //información
-}
-function calcuprecio(){
-    alert("Su pedido ha sido de 35")
-    
 }
